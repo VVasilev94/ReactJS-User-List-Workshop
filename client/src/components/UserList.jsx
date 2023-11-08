fb6d6e0db4f2e1162032879030fea7ac99899cbd
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-11-08 17:54:43 +0200

    creating components

---
+++ client/src/components/UserList.jsx
@@ -0,0 +1,10 @@
+import Search from "./Search";
+
+export default function UserList(props) {
+    return(
+        <section className="card users-container">
+<Search />
+
+        </section>
+    )
+}
